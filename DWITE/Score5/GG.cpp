db6d066597a848dfff4b3795c64940404558553b
Felix <felixfong03@live.com>
2021-01-07 09:17:48 -0500

    Create GG.cpp

---
+++ DWITE/Score5/GG.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mxN = 1e5+1;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    string s; cin>>s;
+    int dif[mxN] {};
+
+    for (int i = 0; i<s.size(); ++i) {
+        dif[i+1] = dif[i]+(s[i]=='G');
+    }
+    
+    int q; cin>>q;
+    int i,j;
+    while (q--) {
+        cin>>i>>j;
+        cout<<dif[j+1]-dif[i]<<'\n';
+    }
+    
+    return 0;
+    
+}
