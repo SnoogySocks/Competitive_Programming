71ea94e33dd9846480c33820306cbaeed263f4f6
Felix <felixfong03@live.com>
2021-01-11 07:41:04 -0500

    Create 18C2S1_An_Array_Problem.cpp

---
+++ CCC/Score7/18C2S1_An_Array_Problem.cpp
@@ -0,0 +1,27 @@
+// https://dmoj.ca/problem/nccc2j3s1
+// 1/2021
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    
+    ios_base::sync_with_stdio(true);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    int n; cin>>n;
+    ll max,sum,i;
+    max = sum = 0;
+    while (n--) {
+        cin>>i;
+        sum += i;
+        if (max<i) max = i;
+    }
+    cout<<min(sum-max, sum/2)<<'\n';
+    
+    return 0;
+    
+}
