e84af7807e5f3ccf40f9fa0e3724fb2781d47421
Felix <felixfong03@live.com>
2021-02-03 07:50:42 -0500

    Create A_Plus_B.cpp

---
+++ DMOJ/Score3/A_Plus_B.cpp
@@ -0,0 +1,18 @@
+// https://dmoj.ca/problem/aplusb
+// 12/2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(0); cin.tie(0);
+    int t; cin>>t;
+    int a,b;
+    while (t--) {
+        cin>>a>>b;
+        cout<<a+b<<'\n';
+    }
+    return 0;
+}
