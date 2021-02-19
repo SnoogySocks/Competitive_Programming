ecf47397b2146189a150777b5c1cd9bf4c846956
Felix <felixfong03@live.com>
2021-02-19 07:16:28 -0500

    Create 21P1_COVID_Party.cpp

---
+++ Contest/UTS_Open/21P1_COVID_Party.cpp
@@ -0,0 +1,70 @@
+// https://dmoj.ca/problem/utso21p1
+// 02/2021
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define uset unordered_set
+#define umap unordered_map
+#define priq priority_queue
+#define all(x) begin(x), end(x)
+
+template<class T, class U> static inline void amax (T& a, U b) { if (a<b) a = b; }
+template<class T, class U> static inline void amin (T& a, U b) { if (a>b) a = b; }
+template<class T> void scan (T& n) { n = 0; bool neg = 0; char c = getchar(); if (c=='-') neg = 1, c = getchar(); for (; c<'0'||'9'<c; c = getchar()); for (; '0'<=c&&c<='9'; c = getchar()) n = (n<<3)+(n<<1)+(c&15); if (neg) n *= -1; }
+template<class T> void scan (T begin, T end) { while (begin!=end) scan(*begin++); }
+void scan (vector<char>& c, const char&& escape = ' ') { char buf; do buf = getchar(); while ((buf<'0'||'9'<buf)&&(buf<'A'||'Z'<buf)&&(buf<'a'||'z'<buf)); for (int i = 0; buf!='\n'&&buf!=escape; buf = getchar()) c.push_back(buf); c.push_back('\0'); }
+template<class T> void print (T n, char&& end = '\n') { bool neg = 0; if (n<0) neg = 1, n *= -1; char snum[65]; int i = 0; do { snum[i++] = n%10+'0'; n /= 10; } while (n); i--; if (neg) putchar('-'); while (i>=0) putchar(snum[i--]); putchar(end); }
+template<class T> void print (T begin, T end) { while (begin!=end) print(*begin++, ' '); putchar('\n'); }
+
+/*
+square size   max people
+1             2
+2             5
+3             8
+4             13
+5             18
+start at 1, add i on oddsquare sizes
+if it is even, increment i by 2 then add i
+
+*/
+
+void solve() {
+
+    int n; scan(n);
+    int max, tracker, size;
+    max = 1;
+    tracker = 1;
+    for (size = 1; max<n; ++size) {
+        if (size%2==0) tracker+=2;
+        max += tracker;
+    }
+    print(size-1);
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#if 0
+    int t; scan(t); while(t--) solve();
+#else
+    solve();
+#endif
+    return 0; 
+}
