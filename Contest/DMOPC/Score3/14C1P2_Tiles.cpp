79b500ea4fef07cb4f5995a8f5474b9153deb508
Felix <felixfong03@live.com>
2021-03-10 08:55:56 -0500

    Create 14C1P2_Tiles.cpp

---
+++ Contest/DMOPC/Score3/14C1P2_Tiles.cpp
@@ -0,0 +1,15 @@
+// https://dmoj.ca/problem/dmopc14c1p2
+// 02/2021
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int w,l,t; cin>>w>>l>>t;
+    cout<<((w/t)*(l/t))<<'\n';
+    
+    return 0;
+    
+}
