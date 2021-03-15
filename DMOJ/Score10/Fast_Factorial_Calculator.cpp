d5e52ebb221e10168b838de955195ea960fd3078
Felix <felixfong03@live.com>
2021-03-15 08:28:30 -0400

    Create Fast_Factorial_Calculator.cpp

---
+++ DMOJ/Score10/Fast_Factorial_Calculator.cpp
@@ -0,0 +1,66 @@
+// https://dmoj.ca/problem/factorial
+// 03/2021
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define uset unordered_set
+#define umap unordered_map
+#define priq priority_queue
+#define vec vector
+#define all(x) begin(x), end(x)
+
+template<class T, class U> static inline void amax (T& a, U b) { if (a<b) a = b; }
+template<class T, class U> static inline void amin (T& a, U b) { if (a>b) a = b; }
+template<class T> void scan (T& n) { n = 0; bool neg = 0; char c = getchar(); if (c=='-') neg = 1, c = getchar(); for (; c<'0'||'9'<c; c = getchar()); for (; '0'<=c&&c<='9'; c = getchar()) n = (n<<3)+(n<<1)+(c&15); if (neg) n *= -1; }
+template<class T> void scan (T begin, T end) { while (begin!=end) scan(*begin++); }
+void scan (vector<char>& c, const char&& escape = ' ') { c.clear(); char buf; do buf = getchar(); while (buf<'!'||'~'<buf); for (; buf!='\n'&&buf!=escape; buf = getchar()) c.push_back(buf); c.push_back('\0'); }
+template<class T> void print (T n, char&& end = '\n') { bool neg = 0; if (n<0) neg = 1, n *= -1; char snum[65]; int i = 0; do { snum[i++] = n%10+'0'; n /= 10; } while (n); i--; if (neg) putchar('-'); while (i>=0) putchar(snum[i--]); putchar(end); }
+template<class T> void print (T begin, T end) { while (begin!=end) print(*begin++, ' '); putchar('\n'); }
+
+const ull mod = 1ull<<32ull;
+const int mxN = 34;
+ull factorial[mxN] {};
+
+void solve() {
+
+    ull n; scan(n);
+
+    // 34! is factorable by 2^32
+    print(n>=mxN?0:factorial[n]);
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#if 1
+
+    // Initialize the factorials
+    factorial[0] = 1ull;
+    for (int cur = 1; cur<mxN; ++cur) {
+        factorial[cur] = factorial[cur-1]*(cur%mod)%mod;
+    }
+
+    int t; scan(t); while(t--) solve();
+#else
+    solve();
+#endif
+    return 0; 
+}
