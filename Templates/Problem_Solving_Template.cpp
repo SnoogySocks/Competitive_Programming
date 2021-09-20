7be619162ed0b1ff32da1f6b54d3929643bbb245
Felix Fong <felixfong03@live.com>
2021-09-20 15:34:26 -0400

    Update Problem_Solving_Template.cpp

--- Templates/Problem_Solving_Template.cpp
+++ Templates/Problem_Solving_Template.cpp
@@ -1,14 +1,9 @@
-/*
-* Fast I/O is a must as the standard iostreams std::cin and std::cout are very slow.
-* Additionally, the scan and print functions were not made by me.
-* Here is where I would put the source if I could remember where I got them from...
-*/
-
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
 #include <string>
 #include <cstring>
+#include <cmath>
 #include <array>
 #include <vector>
 #include <set>
@@ -18,23 +13,26 @@
 #include <stack>
 #include <queue>
 #include <deque>
-// Put #include<bits/stdc++.h> if your IDE has it. Mine doesn't so...
+#include <tuple>
+#include <bitset>
+#include <climits>
 
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+using namespace std; typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull; typedef long double dl; typedef pair<int, int> pii; typedef pair<ll, ll> pll;
 #define uset unordered_set
 #define umap unordered_map
 #define priq priority_queue
+#define vec vector
 #define all(x) begin(x), end(x)
 
 template<class T, class U> static inline void amax (T& a, U b) { if (a<b) a = b; }
 template<class T, class U> static inline void amin (T& a, U b) { if (a>b) a = b; }
-template<class T> void scan (T& n) { n = 0; bool neg = 0; char c = getchar(); if (c=='-') neg = 1, c = getchar(); for (; c<'0'||'9'<c; c = getchar()); for (; '0'<=c&&c<='9'; c = getchar()) n = (n<<3)+(n<<1)+(c&15); if (neg) n *= -1; }
-template<class T> void scan (T begin, T end) { while (begin!=end) scan(*begin++); }
-template<class T> void print (T n, char&& end = '\n') { bool neg = 0; if (n<0) neg = 1, n *= -1; char snum[65]; int i = 0; do { snum[i++] = n%10+'0'; n /= 10; } while (n); i--; if (neg) putchar('-'); while (i>=0) putchar(snum[i--]); putchar(end); }
-template<class T> void print (T begin, T end) { while (begin!=end) print(*begin++, ' '); putchar('\n'); }
+template<class T> static void scan (T& n) { n = 0; bool neg = false; char c = getchar(); if (c=='-') neg = true, c = getchar(); for (; c<'0'||'9'<c; c = getchar()); for (; '0'<=c&&c<='9'; c = getchar()) n = (n<<3)+(n<<1)+(c&15); if (neg) n *= -1; }
+template<class T> static void scan (vector<T>& v, const int&& start = 0) { for (int i = start; i<v.size(); ++i) scan(v[i]); }
+static void scan (vector<char>& c, const char&& escape = ' ', const bool&& add_null_terminator = false) { char buf; do buf = getchar(); while (buf<'!'||'~'<buf); int i; for (i = 0; buf!='\n'&&buf!=escape; buf = getchar()) c[i++] = buf; if (add_null_terminator) c[i] = '\0'; }
+template<class T, class U> static void scan (T& a, U& b) { scan(a); scan(b); } template<class T, class U, class V> static void scan (T& a, U& b, V& c) { scan(a, b); scan(c); } template<class T, class U, class V, class W> static void scan (T& a, U& b, V& c, W& d) { scan(a, b); scan(c, d); }
+template<class T> static void print (T n, char&& end = '\n') { bool neg = false; if (n<0) neg = true, n *= -1; char snum[65]; int i = 0; do { snum[i++] = n%10+'0'; n /= 10; } while (n); i--; if (neg) putchar('-'); while (i>=0) putchar(snum[i--]); putchar(end); }
+template<class T> void print (vector<T>& v) { for (T el:v) print(el, ' '); putchar('\n'); }
+template<class T> void print (vector<T>&& v) { print(v); }
 
 void solve() {
 
@@ -43,11 +41,7 @@ void solve() {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#if 0
-    int t; scan(t); while(t--) solve();
-#else
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
     solve();
-#endif
-    return 0; 
+    return 0;
 }
