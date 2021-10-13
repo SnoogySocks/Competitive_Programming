98f6fdb418f18423fc1af7de70ede9f0ffa92d08
Felix Fong <felixfong03@live.com>
2021-10-13 15:11:00 -0400

    Update Container_With_Most_Water.cpp

--- LeetCode/Curated_List/Container_With_Most_Water.cpp
+++ LeetCode/Curated_List/Container_With_Most_Water.cpp
@@ -4,67 +4,87 @@
 class Solution {
 public:
     
-    struct Container {
-        vector<int> heights;
-        int left, right;
-        Container (vector<int>& heights) 
-            : left(0), right(heights.size()-1), heights(heights) {}
-        Container (vector<int>& heights, int left, int right) 
-            : left(left), right(right), heights(heights) {}
+//     struct Container {
+//         vector<int> heights;
+//         int left, right;
+//         Container (vector<int>& heights) 
+//             : left(0), right(heights.size()-1), heights(heights) {}
+//         Container (vector<int>& heights, int left, int right) 
+//             : left(left), right(right), heights(heights) {}
         
-        void update (int left, int right) {
-            this->left = left;
-            this->right = right;
-        }
-        void update (const Container& c) {
-            update(c.left, c.right);
-        }
+//         void update (int left, int right) {
+//             this->left = left;
+//             this->right = right;
+//         }
+//         void update (const Container& c) {
+//             update(c.left, c.right);
+//         }
         
-        bool shorterIsLeft () {
-            return heights[left]<heights[right];
-        }
+//         bool shorterIsLeft () {
+//             return heights[left]<heights[right];
+//         }
         
-        int getWidth () {
-            return right-left;
-        }
+//         int getWidth () {
+//             return right-left;
+//         }
         
-        int getHeight () {
-            return min(heights[left], heights[right]);
-        }
+//         int getHeight () {
+//             return min(heights[left], heights[right]);
+//         }
         
-        int getArea () {
-            return getWidth()*getHeight();
-        }
+//         int getArea () {
+//             return getWidth()*getHeight();
+//         }
         
-        void maxCont (Container& c) {
-            if (getArea()<c.getArea()) {
-                update(c);
-            }
-        }
+//         void maxCont (Container& c) {
+//             if (getArea()<c.getArea()) {
+//                 update(c);
+//             }
+//         }
         
-        void print () {
-            cout<<left<<' '<<right<<'\n';
-        }
-    };
+//         void print () {
+//             cout<<left<<' '<<right<<'\n';
+//         }
+//     };
     
+//     int maxArea (vector<int>& height) {
+//         int left = 0;
+//         int right = height.size()-1;
+//         Container maxCont(height);
+//         Container curCont(height);
+        
+//         while (left<right) {
+//             if (curCont.shorterIsLeft() && height[curCont.left]<height[++left]) {
+//                 curCont.left = left;
+//                 maxCont.maxCont(curCont);
+//             } else if (!curCont.shorterIsLeft() && height[curCont.right]<height[--right]) {
+//                 curCont.right = right;
+//                 maxCont.maxCont(curCont);
+//             }
+//         }
+        
+//         return maxCont.getArea();
+//     }
+    
+    // A better solution
     int maxArea (vector<int>& height) {
         int left = 0;
         int right = height.size()-1;
-        Container maxCont(height);
-        Container curCont(height);
-        
+        int maxArea = INT_MIN;
+
         while (left<right) {
-            if (curCont.shorterIsLeft() && height[curCont.left]<height[++left]) {
-                curCont.left = left;
-                maxCont.maxCont(curCont);
-            } else if (!curCont.shorterIsLeft() && height[curCont.right]<height[--right]) {
-                curCont.right = right;
-                maxCont.maxCont(curCont);
+            int curArea = min(height[left], height[right])*(right-left);
+            maxArea = max(maxArea, curArea);
+            if (height[left]<height[right]) {
+                ++left;
+            } else {
+                --right;
             }
         }
-        
-        return maxCont.getArea();
+
+        return maxArea;
     }
+    
 };
 // [2,3,4,5,18,17,6]
 // [5,2,12,1,5,3,4,11,9,4]
