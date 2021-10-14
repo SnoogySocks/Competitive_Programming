4b56e6671f4dd13f3e8e88406beafa72f9c9e518
Felix Fong <felixfong03@live.com>
2021-10-14 16:55:07 -0400

    Create Reverse_Bits.cpp

---
+++ LeetCode/Curated_List/Reverse_Bits.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reverse-bits/submissions/
+// October 14 2021
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for (int i = 0; i<31; ++i) {
+            ans |= n&1;
+            ans<<=1;
+            n>>=1;
+        }
+        return ans|(n&1);
+    }
+};
