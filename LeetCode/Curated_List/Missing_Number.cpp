4a1d8f571029cdcd02cf99ba082445d31979e64d
Felix Fong <felixfong03@live.com>
2021-10-14 16:45:09 -0400

    Update Missing_Number.cpp

--- LeetCode/Curated_List/Missing_Number.cpp
+++ LeetCode/Curated_List/Missing_Number.cpp
@@ -3,11 +3,39 @@
 
 class Solution {
 public:
+    // Function to return the XOR of elements
+    // from the range [1, n]
+    int findXOR(int n) {
+        int mod = n % 4;
+
+        // If n is a multiple of 4
+        if (mod == 0)
+            return n;
+
+        // If n % 4 gives remainder 1
+        else if (mod == 1)
+            return 1;
+
+        // If n % 4 gives remainder 2
+        else if (mod == 2)
+            return n + 1;
+
+        // If n % 4 gives remainder 3
+        else if (mod == 3)
+            return 0;
+        
+        return -1;
+    }
     int missingNumber(vector<int>& nums) {
-        int t = 0;
-        for (int i = 0; i<nums.size(); ++i) {
-            t += nums[i];
+        // int t = 0;
+        // for (int i = 0; i<nums.size(); ++i) {
+        //     t += nums[i];
+        // }
+        // return (nums.size()*(nums.size()+1)>>1)-t;
+        int yes = findXOR(nums.size());
+        for (const int& i: nums) {
+            yes ^= i;
         }
-        return (nums.size()*(nums.size()+1)>>1)-t;
+        return yes;
     }
 };
