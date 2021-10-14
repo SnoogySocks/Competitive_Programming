98294abfd15c5214ccbfcc6a75737a8427ced936
Felix Fong <felixfong03@live.com>
2021-10-14 19:40:13 -0400

    Update Longest_Common_Subsequence.cpp

--- LeetCode/Curated_List/Longest_Common_Subsequence.cpp
+++ LeetCode/Curated_List/Longest_Common_Subsequence.cpp
@@ -3,15 +3,15 @@
 
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        /*
-            dp[i][j] = longest common subsequence using
-                       the first i letters from text1 and
-                       the first j letters from text2
-        */
+//     int longestCommonSubsequence(string text1, string text2) {
+//         /*
+//             dp[i][j] = longest common subsequence using
+//                        the first i letters from text1 and
+//                        the first j letters from text2
+//         */
 //         vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1));
-        vector<vector<int>> dp(2, vector<int>(text2.size()+1));
-        for (int i = 1; i<=text1.size(); ++i) {
+        
+//         for (int i = 1; i<=text1.size(); ++i) {
 //             for (int j = 1; j<=text2.size(); ++j) {
 //                 if (text1[i-1]==text2[j-1]) {
 //                     dp[i][j] = dp[i-1][j-1]+1;
@@ -19,6 +19,27 @@ public:
 //                     dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
 //                 }
 //             }
+//         }
+        
+//         // for (int i = 1; i<=text1.size(); ++i) {
+//         //     for (int j = 1; j<=text2.size(); ++j) {
+//         //         cout<<dp[i][j]<<' ';
+//         //     }
+//         //     cout<<'\n';
+//         // }
+        
+//         return dp.back().back();
+//     }
+    
+    // Top-down approach for more efficient memory
+    int longestCommonSubsequence(string text1, string text2) {
+        /*
+            dp[i][j] = longest common subsequence using
+                       the first i letters from text1 and
+                       the first j letters from text2
+        */
+        vector<vector<int>> dp(2, vector<int>(text2.size()+1));
+        for (int i = 1; i<=text1.size(); ++i) {
             for (int j = 1; j<=text2.size(); ++j) {
                 if (text1[i-1]==text2[j-1]) {
                     dp[1][j] = dp[0][j-1]+1;
@@ -40,7 +61,7 @@ public:
         //     cout<<'\n';
         // }
         
-        return dp.back().back();
+        return dp[0].back();
     }
 };
 
