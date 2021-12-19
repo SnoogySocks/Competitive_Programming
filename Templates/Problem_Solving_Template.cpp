7d4c81ceaaae3c4d620c458c51dc220fc178acdd
Felix Fong <felixfong03@live.com>
2021-12-19 10:50:29 -0500

    Update Problem_Solving_Template.cpp

--- Templates/Problem_Solving_Template.cpp
+++ Templates/Problem_Solving_Template.cpp
@@ -17,20 +17,29 @@
 #include <bitset>
 #include <climits>
 
-using namespace std; typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull; typedef long double dl; typedef pair<int, int> pii; typedef pair<ll, ll> pll;
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double dl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 #define uset unordered_set
 #define umap unordered_map
 #define priq priority_queue
 #define vec vector
 #define all(x) begin(x), end(x)
+#define square(x) ((x)*(x))
 
-template<class T> static void scan (T& e) { e = 0; bool neg = false; char c = getchar(); if (c == '-') neg = true, c = getchar(); for (; c < '0' || '9' < c; c = getchar()); for (; '0' <= c && c <= '9'; c = getchar()) e = (e << 3) + (e << 1) + (c & 15); if (neg) e *= -1; }
+template<class T> static void scan (T& e) { e = 0; bool neg = false; char c = getchar(); for (; c<'0' || '9'<c; c = getchar()) if (c=='-') neg = true; for (; '0'<=c && c<='9'; c = getchar()) e = (e<<3)+(e<<1)+(c&15); if (neg) e *= -1; }
 template<class T> static void scan (vector<T>& v, const int&& start = 0) { for (int i = start; i<v.size(); ++i) scan(v[i]); }
-static void scan (vector<char>& c, const char&& escape = ' ') { char buf; do buf = getchar(); while (buf<'!'||'~'<buf); int i; for (i = 0; buf!='\n'&&buf!=escape; buf = getchar()) c[i++] = buf; c[i] = '\0'; }
-template<class T, class U> static void scan (T& a, U& b) { scan(a); scan(b); } template<class T, class U, class V> static void scan (T& a, U& b, V& c) { scan(a, b); scan(c); } template<class T, class U, class V, class W> static void scan (T& a, U& b, V& c, W& d) { scan(a, b); scan(c, d); }
-template<class T> static void print (T e, char&& end = '\n') { bool neg = false; if (e < 0) neg = true, e *= -1; char snum[65]; int i = 0; do { snum[i++] = e % 10 + '0'; e /= 10; } while (e); i--; if (neg) putchar('-'); while (i >= 0) putchar(snum[i--]); putchar(end); }
+static void scan (vector<char>& c, const char&& escape = ' ') { char buf; do buf = getchar(); while (buf<'!' || '~'<buf); int i; for (i = 0; buf!='\n' && buf!=escape; buf = getchar()) c[i++] = buf; }
+template<class T, class U> static void scan (T& a, U& b) { scan(a); scan(b); }
+template<class T, class U, class V> static void scan (T& a, U& b, V& c) { scan(a, b); scan(c); }
+template<class T, class U, class V, class W> static void scan (T& a, U& b, V& c, W& d) { scan(a, b); scan(c, d); }
+template<class T> static void print (T e, char&& end = '\n') { bool neg = false; if (e<0) neg = true, e *= -1; char snum[65]; int i = 0; do { snum[i++] = e%10+'0'; e /= 10; } while (e); i--; if (neg) putchar('-'); while (i>=0) putchar(snum[i--]); putchar(end); }
 static void print (char e, char&& end = '\n') { putchar(e); putchar(end); }
-template<class T> void print (vector<T>& v, char&& end = '\n') { for (const T& el:v) print(el, ' '); putchar(end); }
+template<class T> void print (vector<T>& v, char&& end = '\n') { for (const T& el: v) print(el, ' '); putchar(end); }
 template<class T> void print (vector<T>&& v, char&& end = '\n') { print(v); }
 
 void solve () {
